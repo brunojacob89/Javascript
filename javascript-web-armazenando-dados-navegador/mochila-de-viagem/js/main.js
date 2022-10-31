4c81d3261b6c781524725ee79775e72d038cac77
Bruno Jacob <brunojacob_89@hotmail.com>
2022-10-30 23:27:22 -0300

    atualizando e excluindo

--- javascript-web-armazenando-dados-navegador/mochila-de-viagem/js/main.js
+++ javascript-web-armazenando-dados-navegador/mochila-de-viagem/js/main.js
@@ -1,7 +1,7 @@
 const form = document.getElementById("novoItem"); //Captura o elemento
 const lista = document.getElementById('lista');
-const itens = JSON.parse(localStorage.getItem("itens")) || [];
-
+const itens = JSON.parse(localStorage.getItem("itens")) || [];  // lista dos elementos do LS ||(ou) lista vasia([])
+//JSON.parse transforma a string (guardada no LS) para obj
 
 itens.forEach( (elemento) => {
     criaElemento(elemento)
@@ -13,14 +13,30 @@ form.addEventListener("submit", (evento) =>{ //evento recebe os dados
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
+    const existe = itens.find(elemento => elemento.nome === nome.value) // verifica se existe elemento com o mesmo nome
+    
     const itemAtual = {
         "nome": nome.value,
         "quantidade":quantidade.value
     }
 
-    criaElemento(itemAtual)
 
-    itens.push(itemAtual)
+    if(existe) {
+        itemAtual.id  = existe.id
+        
+        atualizaElemento(itemAtual)
+
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
+    }else {
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1].id + 1) : 0;
+
+        criaElemento(itemAtual)
+
+        itens.push(itemAtual)
+    }
+
+
+    
 
     localStorage.setItem("itens", JSON.stringify(itens)); // JSON.stringify transforma objeto em uma string
 
@@ -38,11 +54,39 @@ function criaElemento(item) {
 
     const numeroItem = document.createElement('strong')
     numeroItem.innerHTML = item.quantidade
+    numeroItem.dataset.id = item.id
 
     novoItem.appendChild(numeroItem)
     novoItem.innerHTML += item.nome
 
-    lista.appendChild(novoItem)
+    novoItem.appendChild(botaoDeleta(item.id))
 
-     
+    lista.appendChild(novoItem)
+    
+}
+
+function atualizaElemento (item) {
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+    
+}
+
+function botaoDeleta(id){
+    const elementoBotao = document.createElement("button")
+    elementoBotao.innerText = "X"
+
+    elementoBotao.addEventListener("click", function(){
+        deletaElemento(this.parentNode, id)
+    })
+
+    return elementoBotao
+}
+
+function deletaElemento(tag, id) { // preciso passar o ID para excluir
+    tag.remove()
+
+    //remove um item do array
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
+
+    localStorage.setItem("itens", JSON.stringify(itens));
+    
 }
